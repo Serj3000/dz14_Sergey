84a1757e45c385bb8aa0d45a98ecf66662d3701a
Serj3000 <52423079+Serj3000@users.noreply.github.com>
2019-09-07 16:10:26 +0300

    layout.blade commit

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -102,53 +102,17 @@
                                     <li><a href="{{route('blog.index')}}">Home</a></li> <!--index.html-->
                                     <li><a href="#">Pages</a>
                                         <ul class="dropdown">
-                                            <li><a href="{{route('blog.index')}}">Home</a></li>
-                                            <li><a href="{{route('blog.archive-blog')}}">Archive Blog</a></li>
-                                            <li><a href="{{route('blog.single-post')}}">Single Post</a></li>
-                                            <li><a href="{{route('blog.about-us')}}">About</a></li>
-                                            <li><a href="{{route('blog.contact')}}">Contact</a></li>
-                                            <li><a href="typography.html">Typography</a></li>
+                                            <li><a href="{{route('blog.index')}}">- Home</a></li>
+                                            <li><a href="{{route('blog.archive-blog')}}">- Archive Blog</a></li>
+                                            <li><a href="{{route('blog.single-post')}}">- Single Post</a></li>
+                                            <li><a href="{{route('blog.about-us')}}">- About</a></li>
+                                            <li><a href="{{route('blog.contact')}}">- Contact</a></li>
+                                            <li><a href="typography.html">- Typography</a></li>
                                         </ul>
                                     </li>
                                     <li><a href="#">Catagories</a>
-                                        <div class="megamenu">
-                                            <ul class="single-mega cn-col-4">
-                                                <li><a href="#">- Features</a></li>
-                                                <li><a href="#">- Food</a></li>
-                                                <li><a href="#">- Travel</a></li>
-                                                <li><a href="#">- Recipe</a></li>
-                                                <li><a href="#">- Bread</a></li>
-                                                <li><a href="#">- Breakfast</a></li>
-                                                <li><a href="#">- Meat</a></li>
-                                                <li><a href="#">- Fastfood</a></li>
-                                                <li><a href="#">- Salad</a></li>
-                                                <li><a href="#">- Soup</a></li>
-                                            </ul>
-                                            <ul class="single-mega cn-col-4">
-                                                <li><a href="#">- Features</a></li>
-                                                <li><a href="#">- Food</a></li>
-                                                <li><a href="#">- Travel</a></li>
-                                                <li><a href="#">- Recipe</a></li>
-                                                <li><a href="#">- Bread</a></li>
-                                                <li><a href="#">- Breakfast</a></li>
-                                                <li><a href="#">- Meat</a></li>
-                                                <li><a href="#">- Fastfood</a></li>
-                                                <li><a href="#">- Salad</a></li>
-                                                <li><a href="#">- Soup</a></li>
-                                            </ul>
-                                            <ul class="single-mega cn-col-4">
-                                                <li><a href="#">- Features</a></li>
-                                                <li><a href="#">- Food</a></li>
-                                                <li><a href="#">- Travel</a></li>
-                                                <li><a href="#">- Recipe</a></li>
-                                                <li><a href="#">- Bread</a></li>
-                                                <li><a href="#">- Breakfast</a></li>
-                                                <li><a href="#">- Meat</a></li>
-                                                <li><a href="#">- Fastfood</a></li>
-                                                <li><a href="#">- Salad</a></li>
-                                                <li><a href="#">- Soup</a></li>
-                                            </ul>
-                                            <ul class="single-mega cn-col-4">
+                                        <div class="dropdown">
+                                            <ul class="single-mega cn-col-12">
                                                 <li><a href="#">- Features</a></li>
                                                 <li><a href="#">- Food</a></li>
                                                 <li><a href="#">- Travel</a></li>
